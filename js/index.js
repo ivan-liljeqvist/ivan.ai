a0f1c069cd5f9f07a11435c93a6dbd4fd6067df1
ivan-liljeqvist <ivanli@kth.se>
2016-04-05 15:25:02 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,58 @@
+$(function() {
+  var data = [
+  { 
+    action: 'type',
+    strings: ["bio Ivan Liljeqvist"],
+    output: '<br><span class="gray">Professional software developer.<br>Love creating things.<br>AI is fascinating.</span><br>&nbsp;',
+    postDelay: 1000
+  },
+  { 
+    action: 'type',
+    strings: ["reach Ivan Liljeqvist"],
+    output: $('.reach-output').html()
+  }
+  
+];
+  runScripts(data, 0);
+});
+
+function runScripts(data, pos) {
+    var prompt = $('.prompt'),
+        script = data[pos];
+    if(script.clear === true) {
+      $('.history').html(''); 
+    }
+    switch(script.action) {
+        case 'type':
+          // cleanup for next execution
+          prompt.removeData();
+          $('.typed-cursor').text('');
+          prompt.typed({
+            strings: script.strings,
+            typeSpeed: 20,
+            callback: function() {
+              var history = $('.history').html();
+              history = history ? [history] : [];
+              history.push('$ ' + prompt.text());
+              if(script.output) {
+                history.push(script.output);
+                prompt.html('');
+                $('.history').html(history.join('<br>'));
+              }
+              // scroll to bottom of screen
+              $('section.terminal').scrollTop($('section.terminal').height());
+              // Run next script
+              pos++;
+              if(pos < data.length) {
+                setTimeout(function() {
+                  runScripts(data, pos);
+                }, script.postDelay || 1000);
+              }
+            }
+          });
+          break;
+        case 'view':
+
+          break;
+    }
+}
